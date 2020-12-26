144f20a2605aaa0a052eeac4a6c99264f09eb935
Kacper Janas <quar5@wp.pl>
2020-12-27 00:02:35 +0100

    added categories implementation

--- main.py
+++ main.py
@@ -13,26 +13,51 @@ def get_active_window():
 
     return _active_window_name
 
+
 def set_time_spend(_active_window_name, time_spend):
 
-        curr_activity = time_tracker.search_for_activity(_active_window_name, time_spend)
+    curr_activity = time_tracker.search_for_activity(_active_window_name, time_spend)
 
-        if curr_activity == None:
-            curr_activity = Activity(_active_window_name, [], time_spend)
-            time_tracker.add_activity(curr_activity)
+    if curr_activity == None:
+        curr_activity = Activity(_active_window_name, [], time_spend)
+        time_tracker.add_activity(curr_activity)
+
+    return curr_activity
+
+
+def update_timer():
+    curr_activity_timer.update_time_spend()
+    return curr_activity_timer.time_spend
+
+
+def update_activity_categories(activity):
+    for category in time_tracker.categories:
+        for keyword in category.keywords:
+            if keyword in activity.name and category.id not in activity.categories_id:
+                activity.add_category(category.id)
+
+
+def create_category(name, wage, keywords):
+    new_category = ActivityCategory(len(time_tracker.categories), name, wage, 0, keywords)
+    if time_tracker.search_category_by_name(new_category.name) == None: # @TODO: Create categories comparator
+        time_tracker.add_category(new_category)
+        time_tracker.update_all_activities_categories()
 
 
 active_window_name = get_active_window()
 activity_name = ""
 time_tracker = TimeTracker()
 curr_activity_timer = Timer()
-
+opened_activities_names = []
 
 try:
     time_tracker.initialize_me()
 except json.decoder.JSONDecodeError:
     print("\nFailed to load data from ./TimeTrackerData.json")
 
+
+create_category("Web", 1, ["Mozilla Firefox", "Google Chrome"])
+create_category("Coding", 2, ["Visual Studio", "Stack Overflow"])
 
 try:
     while True:
@@ -41,16 +66,19 @@ try:
         if active_window_name != new_window_name and new_window_name != "" and active_window_name != "":
             print(active_window_name)
             
-            curr_activity_timer.update_time_spend()
-            time_spend = curr_activity_timer.time_spend
-            set_time_spend(active_window_name, time_spend)                
+            time_spend = update_timer()
+            curr_activity = set_time_spend(active_window_name, time_spend)
+
+            if curr_activity.name not in opened_activities_names:
+                update_activity_categories(curr_activity)
+                opened_activities_names.append(curr_activity.name)
             
             active_window_name = new_window_name
         
         time.sleep(1)
 
 except KeyboardInterrupt:
-    curr_activity_timer.update_time_spend()
-    set_time_spend(active_window_name, curr_activity_timer.time_spend)
+    print(opened_activities_names)
+    set_time_spend(active_window_name, update_timer())
     time_tracker.save_me()
     print("\nData saved! Have a nice day!")
--- time_tracker.py
+++ time_tracker.py
@@ -7,12 +7,17 @@ class TimeTracker:
 
     def __init__(self):
         self.activities = []
+        self.categories = []
 
     def initialize_me(self):
-        if os.path.isfile("./TimeTrackerData.json"):
-            with open("./TimeTrackerData.json", "r") as f:
+        if os.path.isfile("./data/TimeTrackerData.json") and os.path.isfile("./data/TimeTrackerCategories.json"):
+            with open("./data/TimeTrackerData.json", "r") as f:
                 data = json.load(f)
                 self.activities = self.get_activities_from_json(data)
+
+            with open("./data/TimeTrackerCategories.json", "r") as f:
+                data = json.load(f)
+                self.categories = self.get_categories_from_json(data)
         else:
             print("Welcome to Time Tracker!")
 
@@ -20,77 +25,99 @@ class TimeTracker:
         activity_list = []
         for activity in data['activities']:
             activity_list.append(
-                Activity(activity['name'], activity['categories'], activity['total_time_spend'])
+                Activity(activity['name'], activity['categories_id'], activity['total_time_spend'])
             )
         return activity_list
 
+    def get_categories_from_json(self, data):
+        category_list = []
+        for category in data['categories']:
+            category_list.append(ActivityCategory(category['id'], category['name'], category['wage'], category['total_time_spend']))
+        return category_list
+
     def add_activity(self, new_activity):
         self.activities.append(new_activity)
+
+    def add_category(self, new_category):
+        self.categories.append(new_category)
+
+    def update_all_activities_categories(self):
+        for activity in self.activities:
+            for category in self.categories:
+                for keyword in category.keywords:
+                    if keyword in activity.name:
+                        activity.add_category(category.id)
 
     def search_for_activity(self, searched_activity_window_name, time_spend):
         for activity in self.activities:
             if activity.name == searched_activity_window_name:
                 activity.total_time_spend += time_spend
                 return activity
-        
         return None
 
-    def serialize(self):
-        return {
-            "activities": self.serialize_activities_to_json()
-        }
+    def search_category_by_id(self, searched_category_id):
+        for category in self.categories:
+            if category.id == searched_category_id:
+                return category
+        return None
 
-    def serialize_activities_to_json(self):
-        activities_list = []
-        for activity in self.activities:
-            activities_list.append(activity.serialize())
-        return activities_list
+    def search_category_by_name(self, searched_category_name):
+        for category in self.categories:
+            if category.name == searched_category_name:
+                return category
+        return None
+
+    def serialize_list_to_json(self, list_to_serialize):
+        object_list = []
+        for obj in list_to_serialize:
+            object_list.append(obj.serialize())
+        return object_list
+
+    def save_to_json(self, path, serialized_data):
+        with open(path, "w") as f:
+            json.dump(serialized_data, f, indent=4, sort_keys=True)
 
     def save_me(self):
-        with open("./TimeTrackerData.json", "w") as f:
-            json.dump(self.serialize(), f, indent=4, sort_keys=True)
+        self.save_to_json("./data/TimeTrackerData.json", {"activities": self.serialize_list_to_json(self.activities)})
+        self.save_to_json("./data/TimeTrackerCategories.json", {"categories": self.serialize_list_to_json(self.categories)})
 
 
 class ActivityCategory:
-    def __init__(self, _name, _value, _total_time_spend=0):
-        self.name = _name
-        self.value = _value
-        self.total_time_spend = _total_time_spend
 
-    def update_total_time_spend(self, activities):
-        for activity in activities:
-            if(self in activity.categories):
-                self.total_time_spend += activity.total_time_spend
+    def __init__(self, _id, _name, _wage, _total_time_spend=0, _keywords = []):
+        self.id = _id
+        self.name = _name
+        self.wage = _wage
+        self.total_time_spend = _total_time_spend
+        self.keywords = _keywords
 
     def serialize(self):
         return {
+            "id": self.id,
             "name": self.name,
-            "value": self.value
+            "wage": self.wage,
+            "total_time_spend": self.total_time_spend
         }
 
 
 class Activity:
     
-    def __init__(self, _name, _categories, _total_time_spend=0):
+    def __init__(self, _name, _categories_id, _total_time_spend=0):
         self.name = _name
-        self.categories = _categories
+        self.categories_id = _categories_id
         self.total_time_spend = _total_time_spend
 
     def serialize(self):
         return {
             "name": self.name,
-            "categories": self.serialize_categories_to_json(),
+            "categories_id": self.categories_id,
             "total_time_spend" : self.total_time_spend
         }
 
-    def serialize_categories_to_json(self):
-        categories_list = []
-        for category in self.categories:
-            categories_list.append(category.serialize())
-        return categories_list
+    def add_category(self, new_category_id):
+        if new_category_id not in self.categories_id:
+            self.categories_id.append(new_category_id)
 
-    def add_category(self, _new_category):
-        self.categories.push(_new_category)
 
 class Timer:
 
