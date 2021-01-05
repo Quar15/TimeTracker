900054bc1e58c81b0ecc5977b465752e5326f94e
Kacper Janas <quar5@wp.pl>
2021-01-05 23:43:37 +0100

    FIX: Server install matplotlib and flask

--- server/install.sh
+++ server/install.sh
@@ -5,5 +5,5 @@ echo -e "\n Updating System..."
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install Python3 and dependencies
-sudo apt-get install python3 python3-pip
-python3 -m pip install flask numpy matplotlib
+sudo apt-get install python3 python3-pip python3-flask python3-matplotlib -y
+python3 -m pip install flask numpy
