0b063d999d4eb665115ca67d3d1a0ac3152b7816
Kacper Janas <quar5@wp.pl>
2021-01-05 23:36:21 +0100

    FIX: matplotlib error

--- server/install.sh
+++ server/install.sh
@@ -6,4 +6,4 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install Python3 and dependencies
 sudo apt-get install python3 python3-pip
-python3 -m pip install flask numpy python3-matplotlib
+python3 -m pip install flask numpy matplotlib
